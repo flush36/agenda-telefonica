06888eed472a810ea5958dbfa688f0f20a6f6425
Thiago <thiago.empreendedor01@gmail.com>
2019-12-14 12:10:25 -0300

    criando sqlflite e metodo salve e get

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -20,14 +20,33 @@ class ContactHelper {
 
   Future<Database> get db async {
     if(_db != null) {
-      return db;
+      return _db;
     } else {
       _db = await initDb();
       return _db;
     }
   }
 
-    Future<Database> initDb() async {
+ Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+    columns: [idColumn, nameColumn, emailColum, phoneColumn, imgColumn],
+    where: "$idColumn = ?",
+    whereArgs: [id]);
+    if(maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
     final path = join(databasesPath, "contacts.db");
 
